de5ed27391404113b46264fa55ec89d161dd448d
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2023-08-01 00:27:03 -0300

    primer

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="es">
+<head>
+  <meta chartset="utf-8">
+  <title>Primer web</title>
+<link href="estilo.css" rel="stylesheet" type="text/css">--
+
+
+
+</head>
+<body>
+$nom="uñate";
+<?php $nombre = $_GET["ALEJANDRO"];
+print("mi nombre es: ".$nom); ?><br>
+<?php echo "hola mundo, ".$nombre ; ?>
+<p>hola</p>
+
+</body>
+</html>
